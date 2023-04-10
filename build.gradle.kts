64d40e8ba096b9f4ccd6bc33e162fb6e1ee7f870
Krzysztof Zabicki <krzysztof.zabicki@ocado.com>
2023-04-10 21:36:40 +0200

    Add liquibase, add postgres properties file, add initial event model, add sql script creating event table

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,25 @@ repositories {
 }
 
 dependencies {
+    implementation("org.projectlombok:lombok:1.18.22")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation("org.springframework:spring-core:6.0.7")
+
     implementation("org.springframework.boot:spring-boot-starter-web:3.0.5")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.5")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.5")
+
+    //implementation("org.springframework.data:spring-data-cassandra:4.0.4")
+    //implementation("org.springframework.data:spring-data-redis:3.0.4")
+    //implementation("org.springframework.data:spring-data-elasticsearch:5.0.4")
+    //implementation("org.springframework.data:spring-data-neo4j:7.0.4")
+
+    implementation("org.liquibase:liquibase-core:4.20.0")
+    implementation("org.postgresql:postgresql:42.6.0")
 }
 
 tasks.getByName<Test>("test") {
---
+++ src/main/java/pl/zabicki/billing/db/postgres/model/Event.java
@@ -0,0 +1,47 @@
+package pl.zabicki.billing.db.postgres.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String clientId;
+    private String accountId;
+    private String apInstanceId;
+    private String callingNumber;
+    private String calledNumber;
+    private String callingPrefix;
+    private String calledPrefix;
+    private String eventBeginDate;
+    private String eventEndDate;
+    private String productId;
+    private String rootProductId;
+    private long intProperty1;
+    private long intProperty2;
+    private long intProperty3;
+    private long intProperty4;
+    private long intProperty5;
+    private String stringProperty1;
+    private String stringProperty2;
+    private String stringProperty3;
+    private String stringProperty4;
+    private String stringProperty5;
+    private boolean booleanProperty1;
+    private boolean booleanProperty2;
+    private boolean booleanProperty3;
+    private boolean booleanProperty4;
+    private boolean booleanProperty5;
+    private long quantity;
+    private String billingCycleDefId;
+    private String billingCycleInstanceId;
+    private String unit;
+}
